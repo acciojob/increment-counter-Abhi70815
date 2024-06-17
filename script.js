b57652fd19e0148d4b318e4a318b74435b7af62e
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-17 20:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count= document.querySelector("#counter");
+let inc= document.querySelector("#incrementBtn");
+let i=0;
+inc.addEventListener('click', ()=>{
+    i++;
+})
+count.innerText=i;
